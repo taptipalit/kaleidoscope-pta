fd5a003ef26d53768e6726d746a113a96c3504aa
Tapti Palit <tpalit@purdue.edu>
2022-10-30 12:03:37 +0000

    offending PWC situation

---
+++ test/howdoespwcwork/test.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+struct Object {
+    int* field0;
+    int* field1;
+    int* field2;
+};
+
+int a, b, c;
+
+void dothis(void**s, void** p) {
+    *s = *p;
+}
+
+void (*indcall)() = dothis;
+
+int main(void) {
+    struct Object* p = malloc(1024);
+    int** s;
+    p->field0 = &a;
+    p->field1 = &b;
+    p->field2 = &c;
+
+    //int** q = p;
+    //struct Object* p = optr;
+    int** q = &(p->field1);
+    //s = q; // <---- cycle edge
+    p = (struct Object*) s;
+
+    int **r = &(p->field2);
+
+    int* ptr = *r;
+
+    (*indcall)(&s, &q);
+
+    printf("%d\n", *ptr);
+
+    return 0;
+}
